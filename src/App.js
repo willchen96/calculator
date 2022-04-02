c42e9200f9737f2be45f3ffd02e1c40ed757bf0c
Will <weiye.chen96@icloud.com>
2022-04-03 01:34:11 +0800

    Finished caculator app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css"
+import Screen from "./Screen"
+import Keypad from "./Keypad"
+import { KeyProvider } from "./context/KeyContext"
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <div className="background"></div>
+            <div className="body">
+                <KeyProvider>
+                    <Screen />
+                    <Keypad />
+                </KeyProvider>
+            </div>
+        </div>
+    )
 }
 
-export default App;
+export default App
